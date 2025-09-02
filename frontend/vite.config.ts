d3751d76bc807f0fcd8277a6626fd6d3997eaa79
Dillan Choinière <choinieredillan@gmail.com>
2025-09-01 22:28:26 -0400

    setup tanstack router in vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,8 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
 // https://vitejs.dev/config/
 export default defineConfig({
 	resolve: {
@@ -10,5 +10,12 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+		}),
+		react(),
+		tailwindcss(),
+	],
 });
