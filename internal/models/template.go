e309bdf311f9f89fdcd26b825e0979f537945969
Charles-Etienne Gauthier <75446757+charlesGOAT@users.noreply.github.com>
2025-10-25 17:21:30 -0400

    feature: add test and error message

--- internal/models/template.go
+++ internal/models/template.go
@@ -3,5 +3,5 @@ package models
 type Template struct {
 	ID      uint   `gorm:"primaryKey" json:"id"`
 	Name    string `gorm:"size:255;not null;unique" json:"name"`
-	Content string `gorm:"type:text;not null" json:"content"`
+	Content string `gorm:"type:text;not null;" json:"content"`
 }
